329b14fd636c80d454e310649901736135a10002
cecyramos <c.e.c.y_92_@hotmail.com>
2025-08-21 20:36:51 -0400

    consultas

---
+++ consultas.sql
@@ -0,0 +1,42 @@
+-- Consulta 1 --
+/*Mostrar el nombre, telefono y email de todos los clientes que tienen un alquiler activo 
+(es decir, cuya fecha actual esté dentro del rango entre fecha_inicio y fecha_fin).*/
+
+-- Consulta 2 --
+/*Mostrar los vehículos que se alquilaron en el mes de marzo de 2025. Debe mostrar el 
+modelo, marca, y precio_dia de esos vehículos.*/
+
+-- Consulta 3 --
+/*Calcular el precio total del alquiler para cada cliente, considerando el número de 
+días que alquiló el vehículo (el precio por día de cada vehículo multiplicado por la 
+cantidad de días de alquiler).*/
+
+-- Consulta 4 --
+/*Encontrar los clientes que no han realizado ningún pago (no tienen registros en la 
+tabla Pagos). Muestra su nombre y email.*/
+
+-- Consulta 5 --
+/*Calcular el promedio de los pagos realizados por cada cliente. Muestra el nombre del 
+cliente y el promedio de pago.*/
+
+-- Consulta 6 --
+/*Mostrar los vehículos que están disponibles para alquilar en una fecha específica 
+(por ejemplo, 2025-03-18). Debe mostrar el modelo, marca y precio_dia. Si el vehículo 
+está ocupado, no se debe incluir.*/
+
+-- Consulta 7 --
+/*Encontrar la marca y el modelo de los vehículos que se alquilaron más de una vez en 
+el mes de marzo de 2025.*/
+
+-- Consulta 8 --
+/*Mostrar el total de monto pagado por cada cliente. Debe mostrar el nombre del cliente
+ y la cantidad total de pagos realizados (suma del monto de los pagos).*/
+
+-- Consulta 9 --
+/*Mostrar los clientes que alquilaron el vehículo Ford Focus (con id_vehiculo = 3). Debe 
+mostrar el nombre del cliente y la fecha del alquiler.*/
+
+-- Consulta 10 --
+/*Realizar una consulta que muestre el nombre del cliente y el total de días alquilados 
+de cada cliente, ordenado de mayor a menor total de días. El total de días es calculado 
+como la diferencia entre fecha_inicio y fecha_fin.*/
